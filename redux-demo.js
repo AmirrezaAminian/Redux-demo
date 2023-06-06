e618aea340a1150a5a7f06ebc3bab5251cb24946
Amirreza Aminian <aminian.amirreza@gmail.com>
2023-06-06 14:06:11 +0330

    last commit

--- redux-demo.js
+++ redux-demo.js
@@ -1,9 +1,19 @@
 const redux = require('redux') ;
 
 const counterReducer = (state  = {counter : 0}, action) => {
+  if(action.type === 'increment'){
     return {
         counter: state.counter + 1 , 
     }
+  }
+
+  if(action.type === 'decrement'){
+    return {
+      counter : state.counter - 1 ,
+    }
+  }
+
+  return state ;
 }
 
 const store = redux.createStore(counterReducer)
@@ -17,3 +27,4 @@ const counterSubscriber = () => {
 store.subscribe(counterSubscriber)
 
 store.dispatch({type : 'increment'})
+store.dispatch({type:'decrement'})
